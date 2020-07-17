572e259aab252a7b9219ca69872645dc6c7ea18f
Rishikakar <67696019+Rishikakar@users.noreply.github.com>
2020-07-17 15:34:38 +0530

    first commit

---
+++ Models/ApplicationdbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace day4dotnet.Models
+{
+    public class ApplicationdbContext : DbContext
+
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+            optionsBuilder.UseSqlServer("Data Source=DESKTOP-3BG16TQ;user id=sa;password=rishika@;database=day4Db");
+        }
+        public DbSet<User> users { get; set; }
+
+    }
+}
---
+++ Repository/IuserRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using day4dotnet.Models;
+
+namespace day4dotnet.Repository
+{
+    interface IuserRepository
+    {
+        public List<User> Users();
+        public User GetUser(int id);
+        public List<User> DeleteUser(int id);
+        public List<User> ActiveUsers();
+        public List<User> AddUser(User user);
+    }
+
+}
